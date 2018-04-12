e938559c9c1e84e32cf82f453a614390ed28cde2
Christoper <celoranta@gmail.com>
2018-04-11 22:09:04 -0700

    more

--- TaskyBlocks/ViewControllers/ViewController.swift
+++ TaskyBlocks/ViewControllers/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import RealmSwift
 
 class ViewController: UIViewController, UIScrollViewDelegate, TaskDetailDataSource{
-
-
+  
   var activeTaskySet: Set<TaskyNode>!
   var selectedTask: TaskyNode?
   var realm: Realm!
@@ -19,14 +18,13 @@ class ViewController: UIViewController, UIScrollViewDelegate, TaskDetailDataSour
   func serveTaskData() -> (Set<TaskyNode>)
   { return activeTaskySet
   }
-
-  func returnSelectedTask() -> TaskyNode {
-    //This method is an incomplete stub
+  
+  func returnSelectedTask() -> TaskyNode
+  { //This method is an incomplete stub
     print("New Task Returned to Priority VC")
     guard let task = selectedTask
-    else
-    {
-      fatalError("detail view returned a nil task")
+      else
+    { fatalError("detail view returned a nil task")
     }
     return task
   }
@@ -34,33 +32,29 @@ class ViewController: UIViewController, UIScrollViewDelegate, TaskDetailDataSour
   @IBOutlet weak var taskyGraph: TaskyGraphView!
   var tasksData: TaskDataSource?
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  override func viewDidLoad()
+  { super.viewDidLoad()
     taskyGraph.delegate = self
     try! realm = Realm()
     let tasks = realm.objects(TaskyNode.self)
     activeTaskySet = Set(tasks)
-    
     graphIt()
   }
   
   //  Tells the delegate that zooming of the content in the scroll view is about to commence.
   internal func scrollViewWillBeginZooming(_: UIScrollView, with: UIView?)
   {
-  
   }
   
   //  Tells the delegate when zooming of the content in the scroll view completed.
   internal func scrollViewDidEndZooming(_: UIScrollView, with: UIView?, atScale: CGFloat)
-  {
-    print(atScale)
+  { print(atScale)
   }
   
   //  Tells the delegate that the scroll view’s zoom factor changed.
   
   func scrollViewDidZoom(_: UIScrollView)
   {
-    
   }
   
   //
@@ -71,12 +65,10 @@ class ViewController: UIViewController, UIScrollViewDelegate, TaskDetailDataSour
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     switch segue.identifier
-  {
-    case "priorityToDetail":
+    { case "priorityToDetail":
       guard tasksData != nil
         else
-      {
-        fatalError("TasksData is set to nil")
+      { fatalError("TasksData is set to nil")
       }
       //selectedTask = tasksData?.newTask()
       let detailNavController = segue.destination as! UINavigationController
@@ -84,27 +76,27 @@ class ViewController: UIViewController, UIScrollViewDelegate, TaskDetailDataSour
       detailViewController.taskDetailDataSource = self
       detailViewController.tasksData = tasksData
       
-      default:
+    default:
       fatalError("performVC Called an unknown segue")
-  }
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    graphIt()
+    }
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
+  override func viewWillAppear(_ animated: Bool)
+  { graphIt()
+  }
+  
+  override func didReceiveMemoryWarning()
+  { super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
   func graphIt()
   {
-//    guard let tasksDataUnwrapped = tasksData
-//      else
-//    {
-//      fatalError("Data source is nil")
-//    }
+    //    guard let tasksDataUnwrapped = tasksData
+    //      else
+    //    {
+    //      fatalError("Data source is nil")
+    //    }
     let displayTasks = activeTaskySet
     taskyGraph.graphPriorityWith(taskSet: activeTaskySet)
   }
