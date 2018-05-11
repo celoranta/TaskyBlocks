0e1dd829b4d62618ad633c39b59dc0e219c5ee50
Christoper <celoranta@gmail.com>
2018-05-11 15:21:52 -0700

    Employ rudimentary dependence graph

--- TaskyBlocks/ViewControllers/DependenceGraphViewLayout.swift
+++ TaskyBlocks/ViewControllers/DependenceGraphViewLayout.swift
@@ -9,8 +9,9 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
   
   struct seekRegister {
     let task: TaskyNode
-    let ungraphedConsequents: [TaskyNode]
-    let virginal: Bool = true
+    var ungraphedConsequents: [TaskyNode]
+    let isVirginal: Bool = true
+
     
     init(task: TaskyNode)
     {
@@ -19,11 +20,13 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
     }
   }
   
-  var column: Int? = nil
+  var column: CGFloat = 0.0
+  var row: CGFloat = 0.0
   var localDatasource = Array(TaskyNodeEditor.sharedInstance.database)
   var layoutMap = [IndexPath : UICollectionViewLayoutAttributes]()
   var contentSize: CGSize = CGSize.init(width: 1000, height: 1000)
-  var searchingArray: [TaskyNode] = []
+  var seekingArray: [seekRegister] = []
+  var seedTask: TaskyNode!
   
   override var collectionViewContentSize: CGSize {
     return contentSize
@@ -61,20 +64,31 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
   
   override func prepare() {
     super.prepare()
+    
+    //set default values (to be moved to a method)
     contentSize = CGSize.init(width: 1500, height: 1500)
     layoutMap = [:]
+    column = 0.0
+    row = 0.0
+    seekingArray = []
+    seedTask = TaskyNodeEditor.sharedInstance.newTask()
     
-
+    guard let seedTask = seedTask
+      else {
+        fatalError("Error in Seed Task Creation")
+    }
+    TaskyNodeEditor.sharedInstance.changeTitle(task: seedTask, to: "seedTask")
+    TaskyNodeEditor.sharedInstance.updateTaskDescription(for: seedTask, with: "Temporary Task used in the graphing of dependence layout.  Should always be automatically deleted by the graphing process before appearing in the UI")
+    
     let maxDependeesTask = localDatasource.max(by: {dependeeLevelsQty(of: $0) < dependeeLevelsQty(of: $1)})
     var maxLevelsQty = 0
-    if let uMaxDependeesTask = maxDependeesTask
-    {
+    
+    if let uMaxDependeesTask = maxDependeesTask {
       maxLevelsQty = dependeeLevelsQty(of: uMaxDependeesTask)
     }
     
     var dependeeModel: [[TaskyNode]] = []
-    for _ in 0...maxLevelsQty
-    {
+    for _ in 0...maxLevelsQty {
       dependeeModel.append([])
     }
     print("Printing test Empty Data Model \(dependeeModel)")
@@ -84,30 +98,44 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
       dependeeModel[dependeelevelQty].append(task)
     }
     
-    for register in 0..<dependeeModel.count {
-      print("\(register): \(dependeeModel[register].count)")
-    }
-    
-    print("Data model: \(dependeeModel)")
-    
-    var column = 0
-    var row = 0
-    var searchingArray: [TaskyNode] = []
-
-    
     
     for task in dependeeModel[0] {
-      if let indexInDataSource = localDatasource.index(of: task) {
-        let indexPath = IndexPath.init(row: indexInDataSource, section: 0)
-        let attribute = UICollectionViewLayoutAttributes.init(forCellWith: indexPath)
-        attribute.frame.size = self.cellPlotSize
-        attribute.frame.origin = CGPoint.init(x: cellPlotSize.width * CGFloat(indexPath.row), y: 0)
-        layoutMap[indexPath] = attribute
+      TaskyNodeEditor.sharedInstance.add(task: task, asConsequentTo: seedTask)
+    }
+    seekingArray = [(seekRegister.init(task: seedTask))]
+    
+    
+    //Graphing Process
+    
+    while seekingArray.count > 0
+    {
+      let lastIndex = seekingArray.endIndex - 1
+      //remove the last task from the array if it has no consequents
+      if seekingArray[lastIndex].ungraphedConsequents.count == 0 {
+        seekingArray.removeLast()
+        column -= 1
+      }
+      else {
+        column += 1
+        if seekingArray[lastIndex].isVirginal {
+          row += 1
+        }
+        let task = seekingArray[lastIndex].ungraphedConsequents.removeFirst()
+        let graphingRegister = seekRegister.init(task: task)
+        seekingArray.append(graphingRegister)
+        
+        if let indexInDataSource = localDatasource.index(of: task) {
+          let indexPath = IndexPath.init(row: indexInDataSource, section: 0)
+          let attribute = UICollectionViewLayoutAttributes.init(forCellWith: indexPath)
+          attribute.frame.size = self.cellPlotSize
+          attribute.frame.origin = CGPoint.init(x: cellPlotSize.width * column, y: cellPlotSize.height * row)
+          layoutMap[indexPath] = attribute
+        }
       }
     }
-    
-    for register in 0..<dependeeModel.count {
-    }
+    TaskyNodeEditor.sharedInstance.removeAsAntecedentToAll(task: self.seedTask)
+    TaskyNodeEditor.sharedInstance.delete(task: self.seedTask)
+    self.seedTask = nil
   }
   
   fileprivate func dependeeLevelsQty(of task: TaskyNode) -> Int {
