78c107dfdd4e5723dd75e99cd63778dc96f722a5
Christoper <celoranta@gmail.com>
2018-05-10 22:11:38 -0700

    Replace dictionary dependeeModel with array based model

--- TaskyBlocks/ViewControllers/DependenceGraphViewLayout.swift
+++ TaskyBlocks/ViewControllers/DependenceGraphViewLayout.swift
@@ -18,17 +18,14 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
   }
   var initialCellSize = CGSize(width: 100, height: 100)
   var initialCellSpacing = CGFloat.init(0)
-  var cellPlotSize: CGSize
-  {
+  var cellPlotSize: CGSize {
     return CGSize.init(width: initialCellSize.width + initialCellSpacing, height: initialCellSize.height + initialCellSpacing)
   }
   
   override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
     var newAttributes: [UICollectionViewLayoutAttributes] = []
-    for attribute in layoutMap
-    {
-      if attribute.value.frame.intersects(rect)
-      {
+    for attribute in layoutMap {
+      if attribute.value.frame.intersects(rect) {
         newAttributes.append(attribute.value)
       }
     }
@@ -53,47 +50,50 @@ class DependenceGraphViewLayout: GraphCollectionViewLayout, GraphViewLayout {
   override func prepare() {
     contentSize = CGSize.init(width: 1500, height: 1500)
     layoutMap = [:]
-         print("Testing dependee level qty recusion")
-    var dependeeLevelDict: [Int : [TaskyNode]] = [:]
-    for task in localDatasource
+
+    var dependeeModel: [[TaskyNode]] = []
+    let maxDependeesTask = localDatasource.max(by: {dependeeLevelsQty(of: $0) < dependeeLevelsQty(of: $1)})
+    var maxLevelsQty = 0
+    if let uMaxDependeesTask = maxDependeesTask
     {
-      let dependeelevelQty = dependeeLevelsQty(of: task)
-      if dependeeLevelDict[dependeelevelQty] == nil {
-        dependeeLevelDict[dependeelevelQty] = [task]
-      }
-      else {
-        var dependeeLevelRegister = dependeeLevelDict[dependeelevelQty]
-        dependeeLevelRegister!.append(task)
-        dependeeLevelDict[dependeelevelQty] = dependeeLevelRegister!
-      }
+    maxLevelsQty = dependeeLevelsQty(of: uMaxDependeesTask)
     }
-    for register in 0..<dependeeLevelDict.count
+    for _ in 0...maxLevelsQty
     {
-      print("\(register): \(dependeeLevelDict[register]!.count)")
+      dependeeModel.append([])
+    }
+    print("Printing test Empty Data Model \(dependeeModel)")
+    for task in localDatasource {
+      let dependeelevelQty = dependeeLevelsQty(of: task)
+        dependeeModel[dependeelevelQty].append(task)
     }
     
-    for task in localDatasource
-    {
-      if let indexInDataSource = localDatasource.index(of: task)
-      {
-        
+    for register in 0..<dependeeModel.count {
+      print("\(register): \(dependeeModel[register].count)")
+    }
+    
+    print("Data model: \(dependeeModel)")
+    
+    
+    
+    for task in localDatasource {
+      if let indexInDataSource = localDatasource.index(of: task) {
         let indexPath = IndexPath.init(row: indexInDataSource, section: 0)
         let attribute = UICollectionViewLayoutAttributes.init(forCellWith: indexPath)
         attribute.frame.size = self.cellPlotSize
         attribute.frame.origin = CGPoint.init(x: cellPlotSize.height * CGFloat(indexPath.row), y: 0)
         layoutMap[indexPath] = attribute
       }
-      
     }
-
+    
+    for register in 0..<dependeeModel.count {
+      
   }
- 
-  
+  }
+
   fileprivate func dependeeLevelsQty(of task: TaskyNode) -> Int {
-    if task.antecedents.count > 0
-    {
-      for task in task.antecedents
-      {
+    if task.antecedents.count > 0 {
+      for task in task.antecedents {
         return dependeeLevelsQty(of: task) + 1
       }
     }
